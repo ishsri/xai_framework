07de5831af21362504dfea7106498a0a38350ab9
Ishan Srivastava <63843789+ishsri@users.noreply.github.com>
2025-06-07 21:15:45 +0200

    Update grad_cam.py

--- xai_visual_framework_full_release/xai_methods/grad_cam.py
+++ xai_visual_framework_full_release/xai_methods/grad_cam.py
@@ -1,1 +1,9 @@
-# grad-cam implementation
+# xai_methods/grad_cam.py
+
+import torch
+from captum.attr import LayerGradCam
+
+def apply_grad_cam(model, input_tensor, target_class, layer):
+    gradcam = LayerGradCam(model, getattr(model, layer))
+    attr = gradcam.attribute(input_tensor, target=target_class)
+    return attr.squeeze().detach().numpy()
