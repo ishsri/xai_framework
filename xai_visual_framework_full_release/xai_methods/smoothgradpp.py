4ed880321a1a537f80e4fd7b5f2887e85172e6a9
Ishan Srivastava <63843789+ishsri@users.noreply.github.com>
2025-06-07 21:17:04 +0200

    Update smoothgradpp.py

--- xai_visual_framework_full_release/xai_methods/smoothgradpp.py
+++ xai_visual_framework_full_release/xai_methods/smoothgradpp.py
@@ -1,1 +1,12 @@
-# smoothgrad++ implementation
+# xai_methods/smoothgradpp.py
+
+from captum.attr import NoiseTunnel, IntegratedGradients
+
+def apply_smoothgradpp(model, input_tensor, target_class):
+    ig = IntegratedGradients(model)
+    nt = NoiseTunnel(ig)
+    attr = nt.attribute(input_tensor,
+                        nt_type='smoothgrad_sq',
+                        n_samples=20,
+                        target=target_class)
+    return attr.squeeze().detach().numpy()
