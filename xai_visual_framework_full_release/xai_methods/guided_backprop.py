53d2999d14c3673379b5102c1cb5fabfce0f6a58
Ishan Srivastava <63843789+ishsri@users.noreply.github.com>
2025-06-07 21:17:15 +0200

    Update guided_backprop.py

--- xai_visual_framework_full_release/xai_methods/guided_backprop.py
+++ xai_visual_framework_full_release/xai_methods/guided_backprop.py
@@ -1,1 +1,8 @@
-# guided backprop implementation
+# xai_methods/guided_backprop.py
+
+from captum.attr import GuidedBackprop
+
+def apply_guided_backprop(model, input_tensor, target_class):
+    gbp = GuidedBackprop(model)
+    attr = gbp.attribute(input_tensor, target=target_class)
+    return attr.squeeze().detach().numpy()
