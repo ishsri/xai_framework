8febaaf7e1f2c6864ade2fde94059ec277176e92
Ishan Srivastava <63843789+ishsri@users.noreply.github.com>
2025-06-07 21:16:33 +0200

    Update lrp.py

--- xai_visual_framework_full_release/xai_methods/lrp.py
+++ xai_visual_framework_full_release/xai_methods/lrp.py
@@ -1,1 +1,8 @@
-# LRP implementation
+# xai_methods/lrp.py
+
+from captum.attr import LRP
+
+def apply_lrp(model, input_tensor, target_class):
+    lrp = LRP(model)
+    attr = lrp.attribute(input_tensor, target=target_class)
+    return attr.squeeze().detach().numpy()
