5f15e99aeb38f77fa23c9adcee04259d9da0ed38
Ishan Srivastava <63843789+ishsri@users.noreply.github.com>
2025-06-07 21:16:22 +0200

    Update integrated_gradients.py

--- xai_visual_framework_full_release/xai_methods/integrated_gradients.py
+++ xai_visual_framework_full_release/xai_methods/integrated_gradients.py
@@ -1,1 +1,8 @@
-# IG implementation
+# xai_methods/integrated_gradients.py
+
+from captum.attr import IntegratedGradients
+
+def apply_ig(model, input_tensor, target_class):
+    ig = IntegratedGradients(model)
+    attr = ig.attribute(input_tensor, target=target_class, n_steps=50)
+    return attr.squeeze().detach().numpy()
